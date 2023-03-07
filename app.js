6491b39c3aaa4daed6a11110b948bf5bb4b50971
Grimes <snake3773@mail.ru>
2023-03-07 11:35:41 +0300

    module14_homework

--- app.js
+++ app.js
@@ -94,7 +94,7 @@
 //     apiData.forEach(item => {
 //         const cardBlock = `
 //         <div class="card">
-//             <img class="card__img" src="${item.download_url}">
+//             <img class="card__img1" src="${item.download_url}">
 //             <p class="card__title">${item.author}</p>
 //         </div>
 //         `
@@ -113,71 +113,91 @@
 
 // Задание 4
 
-const resultNode = document.querySelector('.result2')
-const inputNode1 = document.querySelector('.input2-width')
-const inputNode2 = document.querySelector('.input2-height')
-const btnNode = document.querySelector('.button2')
+// const resultNode = document.querySelector('.result2')
+// const inputNode1 = document.querySelector('.input2-width')
+// const inputNode2 = document.querySelector('.input2-height')
+// const btnNode = document.querySelector('.button2')
+
+// const useRequest = (url) => {
+//     return fetch(url)
+//         .then((response) => {
+//             return response
+//         })
+//         .then((data) => {
+//             const cardBlock = `
+//                     <div class="card">
+//                         <img class="card__img2" src="${data.url}">
+//                     </div>
+//                     `
+//             resultNode.innerHTML = cardBlock
+//         })
+//         .catch(() => {
+//             console.log('error')
+//         })
+// }
+
+// btnNode.addEventListener('click', async () => {
+//     if (inputNode1.value < 100 || inputNode1.value > 300 || isNaN(inputNode1.value)) {
+//         resultNode.innerHTML = 'одно из чисел вне диапазона от 100 до 300'
+//     } else if (inputNode2.value < 100 || inputNode2.value > 300 || isNaN(inputNode2.value)) {
+//         resultNode.innerHTML = 'одно из чисел вне диапазона от 100 до 300'
+//     } else {
+//         await useRequest(`https://picsum.photos/${inputNode1.value}/${inputNode2.value}`)
+//     }
+// })
+
+// Задание 5
+
+const resultNode = document.querySelector('.result3')
+const inputNumberPageNode = document.querySelector('.input3-number-page')
+const inputLimitNode = document.querySelector('.input3-limit')
+const btnNode = document.querySelector('.button3')
+
+if(localStorage.getItem("images")) {
+    showCards()
+}
 
 const useRequest = (url) => {
     return fetch(url)
-        .then((response) => {
-            return response.json
-        })
-        .then((json) => {
-            const dataApi = json
-            dataApi.forEach(item => {
-                const cardBlock = `
-                        <div class="card">
-                            <img class="card__img" src="${item.download_url}">
-                        </div>
-                        `
-                resultNode.innerHTML = cardBlock
+        .then(response => response.json())
+        .then(data => {
+            let imagesData = []
+            data.forEach(item => {
+                imagesData.push({
+                    imageSrc: item.download_url,
+                })
             })
+            localStorage.setItem("images", imagesData)
+            showCards()
         })
         .catch(() => {
             console.log('error')
         })
 }
 
-// function displayResult(apiData) {
-//     // let cards = ''
+function showCards() {
+    let cards = ''
+    const data = localStorage.getItem("images")
 
-//     apiData.forEach(item => {
-//         const cardBlock = `
-//         <div class="card">
-//             <img class="card__img" src="${item.download_url}">
-//         </div>
-//         `
-//         // cards += cardBlock
-//         resultNode.innerHTML = cardBlock
-//     })
-// }
+    data.forEach(item => {
+        const cardBlock = `
+            <div class="card">
+                <img class="card__img1" src="${item.imageSrc}">
+            </div>
+        `
+        cards += cardBlock
+        resultNode.innerHTML = cards
+    })
+}
 
 btnNode.addEventListener('click', async () => {
-    if (inputNode1.value < 100 || inputNode1.value > 300 && inputNode2.value < 100 || inputNode2.value > 300) {
-        resultNode.innerHTML = 'одно из чисел вне диапазона от 100 до 300'
+    if ((inputNumberPageNode.value < 1 || inputNumberPageNode.value > 10 || isNaN(inputNumberPageNode.value)) && (inputLimitNode.value < 1 || inputLimitNode.value > 10 || isNaN(inputLimitNode.value))) {
+        resultNode.innerHTML = 'Номер страницы и лимит вне диапазона от 1 до 10'
+    } else if (inputNumberPageNode.value < 1 || inputNumberPageNode.value > 10 || isNaN(inputNumberPageNode.value)) {
+        resultNode.innerHTML = 'Номер страницы вне диапазона от 1 до 10'
+    } else if (inputLimitNode.value < 1 || inputLimitNode.value > 10 || isNaN(inputLimitNode.value)) {
+        resultNode.innerHTML = 'Лимит вне диапазона от 1 до 10'
     } else {
-        await useRequest(`https://picsum.photos/${inputNode1.value}/${inputNode2.value}`)
+        await useRequest(`https://picsum.photos/v2/list?page=${inputNumberPageNode.value}&limit=${inputLimitNode.value}`)
     }
 })
-
-// btnNode.addEventListener('click', () => {
-//     fetch('https://picsum.photos/v2/list/?limit=5')
-//         .then(response => {
-//             const result = response.json();
-//             return result;
-//         })
-//         .then(json => {
-//             const apiData = json
-//             apiData.forEach(item => {
-//                 const cardBlock = `
-//                 <div class="card">
-//                     <img class="card__img" src="${item.download_url}">
-//                 </div>
-//                 `
-//                 // cards += cardBlock
-//                 resultNode.innerHTML = cardBlock
-//             })
-//         })
-//         .catch(() => { console.log('error') })
-// })
